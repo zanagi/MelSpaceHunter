62a605c97be578be5f8d7784f66a541530dd6b21
zanagi <kintoki.douji@gmail.com>
2016-01-28 15:44:29 +0200

    Adding screens

---
+++ Screens/Screen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace MelSpaceHunter.Screens
+{
+    abstract class Screen
+    {
+        protected ScreenManager manager;
+
+        public Screen(ScreenManager manager)
+        {
+            this.manager = manager;
+        }
+
+        public abstract void LoadContent(ContentManager content);
+
+        public abstract void Update(GameTime gameTime);
+
+        public abstract void Draw(SpriteBatch spriteBatch);
+
+        protected bool RunTests()
+        {
+            Console.WriteLine("Abstract class Screen called.");
+            return true;
+        }
+    }
+}
---
+++ Screens/ScreenManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MelSpaceHunter.Screens
+{
+    class ScreenManager
+    {
+        // Screens
+        private Stack<Screen> screens;
+
+        // Other
+        private ViewManager viewManager;
+
+        public ScreenManager()
+        {
+            
+        }
+
+        private void Initialize()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
