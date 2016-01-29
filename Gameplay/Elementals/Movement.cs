aae92747d7d73e10d909ca83983c50bc59293344
zanagi <kintoki.douji@gmail.com>
2016-01-29 23:32:37 +0200

    Added Movement enum

---
+++ Gameplay/Elementals/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace MelSpaceHunter.Gameplay.Elementals
+{
+    enum Movements
+    {
+        Straight,
+        Circling,
+        Shaking,
+        Random
+    }
+}
