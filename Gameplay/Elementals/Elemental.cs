800a8244eee889d94d58cb2bbb2b93fd2812c7fb
zanagi <kintoki.douji@gmail.com>
2016-01-29 21:14:34 +0200

    Initialize elementals code

---
+++ Gameplay/Elementals/Elemental.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MelSpaceHunter.Gameplay.Elementals
+{
+    class Elemental
+    {
+        private bool born;
+
+        public Elemental()
+        {
+            this.born = false;
+        }
+    }
+}
---
+++ Gameplay/Elementals/ElementalManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MelSpaceHunter.Gameplay.Elementals
+{
+    class ElementalManager
+    {
+        private List<Elemental> visibleElementals, hiddenElementals;
+
+        public ElementalManager()
+        {
+            this.visibleElementals = new List<Elemental>();
+            this.hiddenElementals = new List<Elemental>();
+        }
+    }
+}
